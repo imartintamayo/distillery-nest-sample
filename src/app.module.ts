5ba1db1e023b61204c0905805c23c111dd2dd4a1
Irving Martin Tamayo <irvingmartintamayo@Irvings-MacBook-Pro.local>
2021-10-25 14:45:23 -0500

    adding swaggwr and mongo config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { environment } from './config/environment';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(environment.mongoConnect),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { environment } from './config/environment';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('DISTILLERY NEST SAMPLE API')
+    .setDescription('The DISTILLERY NEST SAMPLE API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(environment.port);
 }
 bootstrap();
